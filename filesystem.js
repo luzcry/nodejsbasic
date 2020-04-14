3380c1133e75c958db66e7cb94af03cc0d27d5ea
luzcry <tu-email@gmail.com>
2020-04-13 23:58:44 -0500

    crear archivo con filesystem

---
+++ filesystem.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+//el modulo firesystem permite poder trabajar con los archivos del sistema operativo
+
+//crear nuevo archivo
+
+fs.writeFile('./texto.txt', 'linea uno', function (err) {
+    if (err) {
+        console.log(err);
+    }
+    console.log('Archivo creado');
+});
+console.log('ultima linea de codigo');
+
+
+
+//un callback es una funcion que se ejecuta cuando termina el proceso 
--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const os = require('os');
 
 console.log(os.platform());
 console.log(os.release());
-console.log("free mem", os.freemem());
+console.log("free mem", os.freemem(), 'bytes');
+console.log('total mem: ', os.totalmem());
