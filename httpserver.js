01d00e0f2a16f976b9883fd1b69289e3085e5fd5
luzcry <tu-email@gmail.com>
2020-04-14 23:32:24 -0500

    modulo colors

--- httpserver.js
+++ httpserver.js
@@ -1,17 +1,19 @@
 //el modulo http permite hacer peticiones al server para responder la pagina web
 const http = require("http");
 //cuando creo un servidor tenemos que recibir la informacion de los navegadores (request) y cuando reciba la info tiene que responder (response)
+const colors = require("colors");
+
 const handleServer = function (req, res) {
-  res.writeHead(404, {
-    "Content-type": "text/html",
-  });
-  res.write("<h1>Hola mundo</h1>");
-  res.end();
+    res.writeHead(404, {
+        "Content-type": "text/html",
+    });
+    res.write("<h1>Hola mundo</h1>");
+    res.end();
 };
 
 const server = http.createServer(handleServer);
 server.listen(3000, function () {
-  console.log("server on port 3000");
+    console.log("server on port 3000".blue);
 });
 //el metodo listen le dice a que puerto va a escuchar mi servidor
 //una vez inicializado el servidor toma solo el codigo antes de iniciar, hay que reiniciar para que tome los cambios
